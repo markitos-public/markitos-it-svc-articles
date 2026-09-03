9b00aa74618b3311978fd1a8ee62c75c67d3a27e
Marco Antonio - markitos <markitos.es.info@gmail.com>
2026-09-03 23:14:09 +0200

    feat: added CORS

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -61,9 +61,22 @@ func main() {
 
 	address := resolveAddress()
 	log.Printf("Database connectivity OK. Starting REST service on port %s...", address)
-	if err := http.ListenAndServe(address, mux); err != nil {
+	if err := http.ListenAndServe(address, corsMiddleware(mux)); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func connectDatabase() (*gorm.DB, error) {
